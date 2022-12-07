7737d96a99d08b42bce09af3f0f9250314e16ca4
mhb164 <mhb164@gmail.com>
2022-12-07 11:12:35 +0330

    refactored Day07 FirstTry (not much)

--- AdventOfCode2022/Solutions/Day07/Day07FirstTrySolution.cs
+++ AdventOfCode2022/Solutions/Day07/Day07FirstTrySolution.cs
@@ -7,49 +7,82 @@ public class Day07FirstTrySolution : PuzzleSolution
 {
     public override int DayNumber => 07;
 
-    class directory
+    public override string SolvePartOne(in string[] inputLines)
     {
-        public directory Parent { get; set; }
-        public string Name { get; set; }
-        public List<directory> directories { get; set; } = new List<directory>();
-        public List<file> files { get; set; } = new List<file>();
-        public override string ToString() => $"{Name} {GetSize()}";
+        var storage = Parse(inputLines);
+        return storage.all.Values.Where(x => x.Size < 100_000).Sum(x => x.Size).ToString();
+    }
 
-        public string GetFullname()
+    public override string SolvePartTwo(in string[] inputLines)
+    {
+        var storage = Parse(inputLines);
+
+        var freeSpace = 70000000 - storage.root.Size;
+        var needSpace = 30000000 - freeSpace;
+        var target = storage.all.Values.Where(x => x.Size >= needSpace).ToList();
+
+        return target.Min(x => x.Size).ToString();
+    }
+
+    class Directory
+    {
+        public Directory(string name) : this(null, name) { }
+        public Directory(Directory parent, string name)
         {
-            if (Parent != null) return $"{Parent.GetFullname()}\\{Name}";
-            else return Name;
+            Parent = parent;
+            Name = name;
         }
 
-        public int GetSize()
+        public Directory Parent { get; private set; }
+        public string Name { get; private set; }
+        public List<Directory> Directories { get; private set; } = new List<Directory>();
+        public List<File> Files { get; private set; } = new List<File>();
+
+        public override string ToString() => $"{Name} {Size}";
+
+        public string Fullname => Parent != null ? $"{Parent.Fullname}\\{Name}" : Name;
+
+        public int Size
         {
-            var size = 0;
-            if (directories.Count > 0)
-                size = directories.Sum(x => x.GetSize());
-            if (files.Count > 0)
-                size += files.Sum(x => x.Size);
-            return size;
+            get
+            {
+                var size = 0;
+                if (Directories.Count > 0)
+                    size = Directories.Sum(x => x.Size);
+                if (Files.Count > 0)
+                    size += Files.Sum(x => x.Size);
+                return size;
+            }
         }
     }
 
-    class file
+    class File
     {
-        public directory Parent { get; set; }
-        public string Name { get; set; }
-        public int Size { get; set; }
+        public File(Directory directory, string name, int size)
+        {
+            Directory = directory;
+            Name = name;
+            Size = size;
+        }
+
+        public Directory Directory { get; private set; }
+        public string Name { get; private set; }
+        public int Size { get; private set; }
 
         public override string ToString() => $"{Name} {Size}";
 
     }
 
-    public override string SolvePartOne(in string[] inputLines)
+    private static (Directory root, Dictionary<string, Directory> all) Parse(string[] inputLines)
     {
-        var rootDirectory = new directory() { Parent = null, Name = "root" };
-        var allDirectories = new Dictionary<string, directory>();
-        allDirectories.Add(rootDirectory.GetFullname(), rootDirectory);
+        var rootDirectory = new Directory("root");
+        var allDirectories = new Dictionary<string, Directory>
+        {
+            { rootDirectory.Fullname, rootDirectory }
+        };
+
         var currecntDirectory = rootDirectory;
         foreach (var inputLine in inputLines)
-        //for (int i = 0; i < inputLines.Length; i++)
         {
             if (inputLine.StartsWith("$"))
             {
@@ -68,7 +101,7 @@ public class Day07FirstTrySolution : PuzzleSolution
                 else if (inputLine.StartsWith("$ cd"))
                 {
                     var dirName = inputLine.Replace("$ cd", "").Trim();
-                    var dir = currecntDirectory?.directories.FirstOrDefault(d => d.Name == dirName);
+                    var dir = currecntDirectory?.Directories.FirstOrDefault(d => d.Name == dirName);
                     if (dir != null) { currecntDirectory = dir; }
                     else
                     {
@@ -81,12 +114,12 @@ public class Day07FirstTrySolution : PuzzleSolution
                 if (currecntDirectory != null)
                 {
                     var dirName = inputLine.Replace("dir", "").Trim();
-                    var existed = currecntDirectory.directories.FirstOrDefault(x => x.Name == dirName);
+                    var existed = currecntDirectory.Directories.FirstOrDefault(x => x.Name == dirName);
                     if (existed == null)
                     {
-                        var newdirectory = new directory() { Parent = currecntDirectory, Name = dirName };
-                        currecntDirectory.directories.Add(newdirectory);
-                        allDirectories.Add(newdirectory.GetFullname(), newdirectory);
+                        var newdirectory = new Directory(currecntDirectory, dirName);
+                        currecntDirectory.Directories.Add(newdirectory);
+                        allDirectories.Add(newdirectory.Fullname, newdirectory);
                     }
                 }
             }
@@ -96,152 +129,16 @@ public class Day07FirstTrySolution : PuzzleSolution
                 if (currecntDirectory != null && int.TryParse(splitted[0], out var size))
                 {
                     var filename = splitted[1];
-                    var existedfile = currecntDirectory.files.FirstOrDefault(f => f.Name == filename);
+                    var existedfile = currecntDirectory.Files.FirstOrDefault(f => f.Name == filename);
                     if (existedfile == null)
                     {
-                        currecntDirectory.files.Add(new file()
-                        {
-                            Parent = currecntDirectory,
-                            Name = filename,
-                            Size = size
-                        });
+                        currecntDirectory.Files.Add(new File(currecntDirectory, filename, size));
                     }
                 }
             }
         }
 
-        var totals = new Dictionary<HashSet<string>, int>();
-        var all = allDirectories.Values.ToArray();
-        var max = 100000;
-        for (int i = 0; i < all.Length; i++)
-        {
-            var main = all[i];
-            if (main.GetSize() > max) continue;
-            var totalkey = new HashSet<string>();
-            totalkey.Add(main.GetFullname());
-            var totalSize = main.GetSize();
-
-            for (int j = 0; j < all.Length; j++)
-            {
-                if (i == j) continue;
-                var ccc = all[j];
-                if (totalSize + ccc.GetSize() > max) continue;
-
-                totalkey.Add(ccc.GetFullname());
-
-                //if (main.GetFullname().Contains(ccc.GetFullname())) continue;
-                //if (ccc.GetFullname().Contains(main.GetFullname())) continue;
-
-                totalSize += ccc.GetSize();
-            }
-
-            bool totalkeyExisted = false;
-            foreach (var item in totals.Keys)
-            {
-                if (item.Count != totalkey.Count) break;
-                var matchnameCount = 0;
-                foreach (var fullname in item)
-                {
-                    if (totalkey.Contains(fullname))
-                        matchnameCount++;
-                }
-
-                if (matchnameCount == totalkey.Count)
-                {
-                    totalkeyExisted = true;
-                    break;
-                }
-            }
-            if (!totalkeyExisted)
-                totals.Add(totalkey, totalSize);
-        }
-
-        var temp = new Dictionary<string, directory>();
-        foreach (var total in totals)
-            foreach (var item in total.Key)
-            {
-                if (temp.ContainsKey(item)) continue;
-                temp.Add(item, allDirectories[item]);
-            }
-        var maxTotal = temp.Values.Sum(x => x.GetSize());
-
-        return maxTotal.ToString();
-    }
-
-    public override string SolvePartTwo(in string[] inputLines)
-    {
-        var rootDirectory = new directory() { Parent = null, Name = "root" };
-        var allDirectories = new Dictionary<string, directory>();
-        allDirectories.Add(rootDirectory.GetFullname(), rootDirectory);
-        var currecntDirectory = rootDirectory;
-        foreach (var inputLine in inputLines)
-        //for (int i = 0; i < inputLines.Length; i++)
-        {
-            if (inputLine.StartsWith("$"))
-            {
-                if (inputLine.StartsWith("$ cd /"))
-                {
-                    currecntDirectory = rootDirectory;
-                }
-                else if (inputLine.StartsWith("$ ls"))
-                {
-                    continue;
-                }
-                else if (inputLine.StartsWith("$ cd .."))
-                {
-                    currecntDirectory = currecntDirectory?.Parent ?? rootDirectory;
-                }
-                else if (inputLine.StartsWith("$ cd"))
-                {
-                    var dirName = inputLine.Replace("$ cd", "").Trim();
-                    var dir = currecntDirectory?.directories.FirstOrDefault(d => d.Name == dirName);
-                    if (dir != null) { currecntDirectory = dir; }
-                    else
-                    {
-
-                    }
-                }
-            }
-            else if (inputLine.StartsWith("dir"))
-            {
-                if (currecntDirectory != null)
-                {
-                    var dirName = inputLine.Replace("dir", "").Trim();
-                    var existed = currecntDirectory.directories.FirstOrDefault(x => x.Name == dirName);
-                    if (existed == null)
-                    {
-                        var newdirectory = new directory() { Parent = currecntDirectory, Name = dirName };
-                        currecntDirectory.directories.Add(newdirectory);
-                        allDirectories.Add(newdirectory.GetFullname(), newdirectory);
-                    }
-                }
-            }
-            else
-            {
-                var splitted = inputLine.Split(" ");
-                if (currecntDirectory != null && int.TryParse(splitted[0], out var size))
-                {
-                    var filename = splitted[1];
-                    var existedfile = currecntDirectory.files.FirstOrDefault(f => f.Name == filename);
-                    if (existedfile == null)
-                    {
-                        currecntDirectory.files.Add(new file()
-                        {
-                            Parent = currecntDirectory,
-                            Name = filename,
-                            Size = size
-                        });
-                    }
-                }
-            }
-        }
-
-        var freeSpace = 70000000 - rootDirectory.GetSize();
-        var needSpace = 30000000 - freeSpace;
-        //var targetdd = string.Join("\r\n", allDirectories.Values.OrderBy(x => x.GetSize()).Select(x => $"{x.GetFullname()} {x.GetSize()}"));
-        var target = allDirectories.Values.Where(x => x.GetSize() >= needSpace).ToList(); ;
-
-        return target.Min(x => x.GetSize()).ToString();
+        return (rootDirectory, allDirectories);
     }
 
 }
