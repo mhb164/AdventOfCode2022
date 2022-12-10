58f4514a2e8dcdfd9e77012c6ed1d75bc4982f19
mhb164 <mhb164@gmail.com>
2022-12-10 10:33:53 +0330

    add Day10 FirstTry Solution :) update README.md

--- AdventOfCode2022/Puzzles/Day10Puzzle.cs
+++ AdventOfCode2022/Puzzles/Day10Puzzle.cs
@@ -10,15 +10,15 @@ namespace AdventOfCode2022.Puzzles;
 public class Day10Puzzle : Puzzle
 {
     public override int DayNumber => 10;
-    public override string Title => "";
+    public override string Title => "Cathode-Ray Tube";
 
     public override string[] SampleInput =>
-        ""
+        "addx 15\r\naddx -11\r\naddx 6\r\naddx -3\r\naddx 5\r\naddx -1\r\naddx -8\r\naddx 13\r\naddx 4\r\nnoop\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx -35\r\naddx 1\r\naddx 24\r\naddx -19\r\naddx 1\r\naddx 16\r\naddx -11\r\nnoop\r\nnoop\r\naddx 21\r\naddx -15\r\nnoop\r\nnoop\r\naddx -3\r\naddx 9\r\naddx 1\r\naddx -3\r\naddx 8\r\naddx 1\r\naddx 5\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\naddx -36\r\nnoop\r\naddx 1\r\naddx 7\r\nnoop\r\nnoop\r\nnoop\r\naddx 2\r\naddx 6\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\naddx 1\r\nnoop\r\nnoop\r\naddx 7\r\naddx 1\r\nnoop\r\naddx -13\r\naddx 13\r\naddx 7\r\nnoop\r\naddx 1\r\naddx -33\r\nnoop\r\nnoop\r\nnoop\r\naddx 2\r\nnoop\r\nnoop\r\nnoop\r\naddx 8\r\nnoop\r\naddx -1\r\naddx 2\r\naddx 1\r\nnoop\r\naddx 17\r\naddx -9\r\naddx 1\r\naddx 1\r\naddx -3\r\naddx 11\r\nnoop\r\nnoop\r\naddx 1\r\nnoop\r\naddx 1\r\nnoop\r\nnoop\r\naddx -13\r\naddx -19\r\naddx 1\r\naddx 3\r\naddx 26\r\naddx -30\r\naddx 12\r\naddx -1\r\naddx 3\r\naddx 1\r\nnoop\r\nnoop\r\nnoop\r\naddx -9\r\naddx 18\r\naddx 1\r\naddx 2\r\nnoop\r\nnoop\r\naddx 9\r\nnoop\r\nnoop\r\nnoop\r\naddx -1\r\naddx 2\r\naddx -37\r\naddx 1\r\naddx 3\r\nnoop\r\naddx 15\r\naddx -21\r\naddx 22\r\naddx -6\r\naddx 1\r\nnoop\r\naddx 2\r\naddx 1\r\nnoop\r\naddx -10\r\nnoop\r\nnoop\r\naddx 20\r\naddx 1\r\naddx 2\r\naddx 2\r\naddx -6\r\naddx -11\r\nnoop\r\nnoop\r\nnoop"
         .ToLines();
 
-    public override string PartOneSampleAnswer => "?";
-    public override string PartTwoSampleAnswer => "?";
+    public override string PartOneSampleAnswer => "13140";
+    public override string PartTwoSampleAnswer => "";
 
-    public override string PartOneActualAnswer => "?";
-    public override string PartTwoActualAnswer => "?";
+    public override string PartOneActualAnswer => "12460";
+    public override string PartTwoActualAnswer => "EZFPRAKL";
 }
--- AdventOfCode2022/Solutions/Day10/Day10FirstTrySolution.cs
+++ AdventOfCode2022/Solutions/Day10/Day10FirstTrySolution.cs
@@ -1,28 +1,124 @@
-﻿namespace AdventOfCode2022.Solutions;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2022.Solutions;
 
 [PuzzleDayNumber(10)]
 public class Day10FirstTrySolution : PuzzleSolution
 {
     public override int DayNumber => 10;
+
+    enum InsType { nope, addx }
+    class Ins
+    {
+        public InsType Type;
+        public int value;
+        public int state;
+
+        internal static Ins Parse(string line)
+        {
+            if (line.StartsWith("noop")) return new Ins() { Type = InsType.nope, state = 1 };
+            else return new Ins() { Type = InsType.addx, value = int.Parse(line[4..]), state = 2 };
+        }
+    }
+
+    class Cpu
+    {
+        public int X { get; private set; } = 1;
+        public int Cycle { get; private set; } = 1;
+        public readonly List<Ins> InsSet;
+        int insIndex = 0;
+        public string output = "";
+
+        public Cpu(List<Ins> insSet)
+        {
+            InsSet = insSet;
+        }
+
+        internal bool DoCycle()
+        {
+            if (insIndex >= InsSet.Count) return false;
+            var ins = InsSet[insIndex];
+            if (ins.state == 0)
+            {
+                insIndex++;
+                if (insIndex >= InsSet.Count) return false;
+                ins = InsSet[insIndex];
+            }
+
+            if (ins.Type == InsType.nope)
+            {
+                DrawOnCRT();
+                Cycle++;
+                ins.state = 0;
+            }
+            else //if (ins.Type == InsType.addx)
+            {
+                if (ins.state == 2)
+                {
+                    DrawOnCRT();
+                    Cycle++;
+                    ins.state = 1;
+                }
+                else
+                {
+                    DrawOnCRT();
+                    Cycle++;
+                    X += ins.value;
+                    ins.state = 0;
+                }
+            }
+            return true;
+        }
+
+        private void DrawOnCRT()
+        {            
+            var p = output.Length % 40;
+            var dd = X - p+1;
+            if (dd == 0 || dd ==1 || dd == 2)
+                output += "#";
+            else
+                output += ".";
+        }
+    }
     public override object SolvePartOne(in string[] inputLines)
     {
         var result = string.Empty;
-        //foreach (var inputLine in inputLines)
-        for (int i = 0; i < inputLines.Length; i++)
-        {
+        var set = inputLines.Select(x => Ins.Parse(x)).ToList();
+        var cpu = new Cpu(set);
 
+        var targets = new List<int>() { 20, 60, 100, 140, 180, 220 };
+        var targetssignals = new List<(int, int, int)>();
+        while (cpu.DoCycle())
+        {
+            if (targets.Contains(cpu.Cycle))
+            {
+                targetssignals.Add(new(cpu.Cycle * cpu.X, cpu.Cycle, cpu.X));
+            }
         }
-        return result;
+
+        return targetssignals.Sum(x => x.Item1);
     }
 
     public override object SolvePartTwo(in string[] inputLines)
     {
         var result = string.Empty;
-        //foreach (var inputLine in inputLines)
-        for (int i = 0; i < inputLines.Length; i++)
-        {
+        var set = inputLines.Select(x => Ins.Parse(x)).ToList();
+        var cpu = new Cpu(set);
 
+        while (cpu.DoCycle())
+        {
         }
+
+        Console.WriteLine(cpu.output.Length);
+        Console.WriteLine(cpu.output[00..39]);
+        Console.WriteLine(cpu.output[40..79]);
+        Console.WriteLine(cpu.output[80..119]);
+        Console.WriteLine(cpu.output[120..159]);
+        Console.WriteLine(cpu.output[160..199]);
+        Console.WriteLine(cpu.output[200..239]);
+
         return result;
     }
 
