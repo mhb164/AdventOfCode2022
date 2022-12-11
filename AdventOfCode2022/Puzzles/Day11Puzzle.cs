b52c240bdea338a0f6389452feef510886e15443
mhb164 <mhb164@gmail.com>
2022-12-11 14:30:16 +0330

    add Day11 FirstTry Solution :)

--- AdventOfCode2022/Puzzles/Day11Puzzle.cs
+++ AdventOfCode2022/Puzzles/Day11Puzzle.cs
@@ -10,15 +10,15 @@ namespace AdventOfCode2022.Puzzles;
 public class Day11Puzzle : Puzzle
 {
     public override int DayNumber => 11;
-    public override string Title => "";
+    public override string Title => "Monkey in the Middle";
 
     public override string[] SampleInput =>
-        ""
+        "Monkey 0:\r\n  Starting items: 79, 98\r\n  Operation: new = old * 19\r\n  Test: divisible by 23\r\n    If true: throw to monkey 2\r\n    If false: throw to monkey 3\r\n\r\nMonkey 1:\r\n  Starting items: 54, 65, 75, 74\r\n  Operation: new = old + 6\r\n  Test: divisible by 19\r\n    If true: throw to monkey 2\r\n    If false: throw to monkey 0\r\n\r\nMonkey 2:\r\n  Starting items: 79, 60, 97\r\n  Operation: new = old * old\r\n  Test: divisible by 13\r\n    If true: throw to monkey 1\r\n    If false: throw to monkey 3\r\n\r\nMonkey 3:\r\n  Starting items: 74\r\n  Operation: new = old + 3\r\n  Test: divisible by 17\r\n    If true: throw to monkey 0\r\n    If false: throw to monkey 1"
         .ToLines();
 
-    public override string PartOneSampleAnswer => "?";
-    public override string PartTwoSampleAnswer => "?";
+    public override string PartOneSampleAnswer => "10605";
+    public override string PartTwoSampleAnswer => "2713310158";
 
-    public override string PartOneActualAnswer => "?";
-    public override string PartTwoActualAnswer => "?";
+    public override string PartOneActualAnswer => "88208";
+    public override string PartTwoActualAnswer => "21115867968";
 }
--- AdventOfCode2022/Solutions/Day11/Day11FirstTrySolution.cs
+++ AdventOfCode2022/Solutions/Day11/Day11FirstTrySolution.cs
@@ -1,29 +1,143 @@
-﻿namespace AdventOfCode2022.Solutions;
+﻿using System.Reflection.Emit;
+
+namespace AdventOfCode2022.Solutions;
 
 [PuzzleDayNumber(11)]
 public class Day11FirstTrySolution : PuzzleSolution
 {
     public override int DayNumber => 11;
-    public override object SolvePartOne(string[] lines)
+
+    class PackItem
     {
-        var result = string.Empty;
-        //foreach (var line in lines)
+        public PackItem(long value) { WorryLevel = value; }
+        public long WorryLevel;
+
+        public override string ToString() => WorryLevel.ToString();
+    }
+
+    class Monkey
+    {
+        public int Number;
+        public List<PackItem> Items = new List<PackItem>();
+        public Func<long, long> Operation;
+        public long TestParameter;
+
+        public int TestTrueMonkeyNumber;
+        public Monkey TestTrueMonkey;
+
+        public int TestFalseMonkeyNumber;
+        public Monkey TestFalseMonkey;
+
+        public long ItemInspectCounter = 0;
+
+        public override string ToString() => $"Monkey[{Number}] (Inspect Count: {ItemInspectCounter}) {string.Join(", ", Items)}";
+        //public override string ToString() => $"Monkey[{Number}] (Inspect Count: {ItemInspectCounter})";
+
+        internal void Play(long worrylevel)
+        {
+            if (Items.Count == 0) return;
+
+            var items = Items.ToList();
+            foreach (var item in items)
+            {
+                ItemInspectCounter++;
+                Items.Remove(item);
+                item.WorryLevel = Operation(item.WorryLevel) / worrylevel;
+
+                if (item.WorryLevel % TestParameter == 0)
+                {
+                    TestTrueMonkey.Items.Add(item);
+                }
+                else
+                {
+                    TestFalseMonkey.Items.Add(item);
+                }
+
+            }
+        }
+    }
+
+    private Dictionary<int, Monkey> Parse(string[] lines)
+    {
+        var monkeys = new Dictionary<int, Monkey>();
         for (int i = 0; i < lines.Length; i++)
         {
+            if (!lines[i].StartsWith("Monkey "))
+                continue;
 
+            var monkey = new Monkey();
+            monkey.Number = int.Parse(lines[i].Replace("Monkey ", "").Replace(":", ""));
+            monkey.Items = lines[i + 1].Replace("  Starting items: ", "").Split(',').Select(x => new PackItem(long.Parse(x))).ToList();
+
+            var oplineItems = lines[i + 2].Replace("Operation: new = ", "").Trim().Split(' ');
+            monkey.Operation = (long old) =>
+            {
+                var p1 = (oplineItems[0] == "old") ? old : long.Parse(oplineItems[0]);
+
+                var p2 = (oplineItems[2] == "old") ? old : long.Parse(oplineItems[2]);
+
+                return oplineItems[1] switch
+                {
+                    "+" => p1 + p2,
+                    "-" => p1 - p2,
+                    "*" => p1 * p2,
+                    "/" => p1 / p2,
+                    _ => throw new NotImplementedException(),
+                };
+            };
+
+            monkey.TestParameter = int.Parse(lines[i + 3].Replace("  Test: divisible by ", ""));
+            monkey.TestTrueMonkeyNumber = int.Parse(lines[i + 4].Replace("If true: throw to monkey ", ""));
+            monkey.TestFalseMonkeyNumber = int.Parse(lines[i + 5].Replace("If false: throw to monkey ", ""));
+            monkeys.Add(monkey.Number, monkey);
         }
-        return result;
+
+        foreach (var monkey in monkeys.Values)
+        {
+            monkey.TestTrueMonkey = monkeys[monkey.TestTrueMonkeyNumber];
+            monkey.TestFalseMonkey = monkeys[monkey.TestFalseMonkeyNumber];
+        }
+        return monkeys;
+    }
+
+    public override object SolvePartOne(string[] lines)
+    {
+        var monkeys = Parse(lines).Values.ToList();
+        for (int i = 0; i < 20; i++)
+        {
+            Play(monkeys, 3);
+        }
+        var ItemInspectCounters = monkeys.OrderByDescending(x => x.ItemInspectCounter).Select(x => x.ItemInspectCounter).ToList();
+        return ItemInspectCounters[0] * ItemInspectCounters[1];
+    }
+
+    private void Play(List<Monkey> monkeys, long worrylevel)
+    {
+        foreach (var monkey in monkeys)
+        {
+            monkey.Play(worrylevel);
+        }
     }
 
     public override object SolvePartTwo(string[] lines)
     {
-        var result = string.Empty;
-        //foreach (var line in lines)
-        for (int i = 0; i < lines.Length; i++)
-        {
+        var monkeys = Parse(lines).Values.ToList();
 
+        var multiplier = (long coefficient, Monkey monkey) => coefficient * monkey.TestParameter;
+        var coefficient = monkeys.Aggregate(1L, multiplier);
+
+        for (int i = 0; i < 10_000; i++)
+        {
+            foreach (var monkey in monkeys)
+                foreach (var item in monkey.Items)
+                {
+                    item.WorryLevel %= coefficient;
+                }
+            Play(monkeys, 1);
         }
-        return result;
+        var ItemInspectCounters = monkeys.OrderByDescending(x => x.ItemInspectCounter).Select(x => x.ItemInspectCounter).ToList();
+        return ItemInspectCounters[0] * ItemInspectCounters[1];
     }
+
 
 }
